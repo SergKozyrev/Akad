2d1d60a84446d63db37eec42e3cb694054fa7ddd
Sergey Kozyrev <serhii.kozyriev@gmail.com>
2019-04-26 12:25:25 +0300

    create mobile version

--- grid.js
+++ grid.js
@@ -2,12 +2,12 @@ let smartgrid = require('smart-grid');
 
 let settings = {
     outputStyle: 'scss',
-    columns: 12, 
-    offset: '30px', 
-    mobileFirst: false, 
+    columns: 12,
+    offset: '30px',
+    mobileFirst: false,
     container: {
-        maxWidth: '1170px', 
-        fields: '15px' 
+        maxWidth: '1170px',
+        fields: '15px'
     },
     breakPoints: {
         lg: {
@@ -21,8 +21,11 @@ let settings = {
         },
         xs: {
             width: '576px'
+        },
+        xxs: {
+            width: '360px'
         }
     }
 };
- 
+
 smartgrid('./src/sass/sass_components', settings);
